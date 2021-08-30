1c0a667832bf72c94873241aca39a3d7efc245a3
Sethhhhhhh <yohannvvt@gmail.com>
2021-08-30 06:59:06 +0200

    parsing...

--- microshell.c
+++ microshell.c
@@ -1,19 +1,104 @@
 #include "microshell.h"
 
-int parse(t_args **args, char *av) {
-    
-    
-    
-    return (0);
+static size_t	s_strlen(char *str) {
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (i);
+	while (str[i])
+		i++;
+	return (i);
 }
 
-int main(int ac, char **av) {
-    t_args  *args;
+static char	*s_strdup(char *str) {
+	char	*new;
+	size_t 	i;
 
-    args = NULL;
-    for (unsigned int i = 1; i < ac; i++) {
-        parse(&args, av[i]);
-    }
+	new = (char *) malloc(sizeof(char) * (s_strlen(str) + 1));
+	if (!new)
+		return (NULL);
+	for (i = 0; str[i]; i++) {
+		new[i] = str[i];
+	}
+	new[i] = 0;
+	return (new);
+}
 
-    return (0);
+static int	add_arg(t_stack *stack, char *arg) {
+	char			**tmp;
+	unsigned int	i;
+
+	tmp = (char **) malloc(sizeof(char *) * (stack->size + 2));
+	if (!tmp)
+		return (1);
+	for (i = 0; i < stack->size; i++)
+		tmp[i] = stack->args[i];
+	if (stack->size > 0)
+		free(stack->args);
+	tmp[i++] = s_strdup(arg);
+	tmp[i] = 0;
+	stack->args = tmp;
+	stack->size += 1;
+	return (0);
+}
+
+static char	push(t_stack **stack, char *arg) {
+	t_stack	*new;
+
+	new = (t_stack *) malloc(sizeof(t_stack));
+	if (!new)
+		return (1);
+	new->args = NULL;
+	new->type = TYPE_END;
+	new->previous = NULL;
+	new->next = NULL;
+	if (*stack) {
+		(*stack)->next = new;
+		new->previous = *stack;
+	}
+	*stack = new;
+	return (add_arg(new, arg));
+}
+
+static char	parse(t_stack **stack, char *arg) {
+	char    sep;
+
+	sep = !strncmp(";", arg, 1);
+	if (sep && !(*stack))
+		return (0);
+	if (!sep)
+		push(stack, arg);
+	if (sep)
+		(*stack)->type = TYPE_BREAK;
+
+	return (0);
+}
+
+int	main(int ac, char **av, char **env) {
+	t_stack  *stack;
+
+	stack = NULL;
+	for (unsigned int i = 1; i < ac; i++) {
+		parse(&stack, av[i]);
+	}
+	while (stack->previous)
+		stack = stack->previous;
+	while (stack) {
+
+		printf("cmd:  ");
+		for (size_t i = 0; stack->args[i]; i++) {
+			printf("%s", stack->args[i]);
+		}
+		printf(" | type: ");
+		if (stack->type == TYPE_BREAK)
+			printf("TYPE_BREAK\n");
+		else if (stack->type == TYPE_END)
+			printf("TYPE_END\n");
+		else if (stack->type == TYPE_PIPE)
+			printf("TYPE_PIPE\n");
+		stack = stack->next;
+	}
+
+	return (0);
 }
--- microshell.h
+++ microshell.h
@@ -1,16 +1,35 @@
 #ifndef __MICROSHELL_H__
 #define __MICROSHELL_H__
 
+/* Header(s) */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
-typedef struct  s_args {
-    char    **args;
-    char    type;
+/* Define(s) */
 
-    struct s_args   *previous;
-    struct s_args   *next;
-}               t_args;
+#define SIDE_OUT    0
+#define SIDE_IN     1
+
+#define STDIN       0
+#define STDOUT      1
+#define STDERR      2
+
+#define TYPE_END    0
+#define TYPE_PIPE   1
+#define TYPE_BREAK  2
+
+/* Structure(s) */
+
+typedef struct  s_stack {
+    char            **args;
+    char            type;
+    unsigned int    size;
+
+    struct s_stack  *previous;
+    struct s_stack  *next;
+}               t_stack;
 
 #endif
